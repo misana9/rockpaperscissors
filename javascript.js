e2d7db11b212da6d3a018bfcf7e120160e8ec543
JD <once.jd2@gmail.com>
2024-07-02 11:23:27 +0800

    added human choice, added play round function, added playgame function, calculates the winner of the game. done all

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * 3);
     if(computerChoice == 0){
@@ -11,5 +12,71 @@ function getComputerChoice(){
     }
 }
 
+function getHumanChoice(){
+    let userInput = prompt("Whats your play?");
+    return userInput.toLowerCase();
+}
 
-console.log(getComputerChoice(3))
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    function playRound(humanChoice, computerChoice){
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+    
+        if(humanChoice == "rock"){
+            if (computerChoice == "scissor"){
+                console.log("You win! Rock beats scissor.");
+                humanScore = humanScore + 1;
+            }
+            else if(computerChoice == "paper"){
+                console.log("You lose! Paper beats rock");
+                computerScore = computerScore + 1;
+            }
+            else{
+                console.log("Its a draw")
+            }
+        }
+        else if(humanChoice == "scissor"){
+            if (computerChoice == "rock"){
+                console.log("You lose! Rock beats scissor.");
+                computerScore = computerScore + 1;
+            }
+            else if(computerChoice == "paper"){
+                console.log("You win! Scissor beats paper");
+                humanScore = humanScore + 1;
+            }
+            else{
+                console.log("Its a draw")
+            }
+        }
+        else if(humanChoice == "paper"){
+            if (computerChoice == "rock"){
+                console.log("You win! Paper beats rock.");
+                humanScore = humanScore + 1;
+            }
+            else if(computerChoice == "scissor"){
+                console.log("You lose! Scissor beats paper");
+                computerScore = computerScore + 1;
+            }
+            else{
+                console.log("Its a draw")
+            }
+        }
+    }
+
+
+    for(let i = 0;i < 5; i++){
+        playRound();
+    }
+
+    if(humanScore > computerScore)
+        console.log('You won against a computer!')
+    else if(humanScore < computerScore)
+        console.log('You lost to a computer!')
+    else
+        console.log("It's a draw!");
+}
+
+console.log(playGame())
